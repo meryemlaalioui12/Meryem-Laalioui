43ff98978e480506e458f407327db56bb3256830
Meryem Laalioui <meryemlaalioui092@gmail.com>
2025-08-06 21:02:58 -0700

    Update title and project name to Meryem Laalioui

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Meryem Laalioui',
+  description: 'Professional QR CV Generator - Meryem Laalioui',
+  generator: 'Next.js',
 }
 
 export default function RootLayout({
